75b37e9074c70b461c095cd59b26007080cab616
LeffeMannen <andreas.ohlsson@elev.ga.ntig.se>
2020-08-29 19:05:54 +0200

    Update Program.cs

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,6 +7,27 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Niklas!");
+
+
+
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+
+
+
+            Console.WriteLine("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+
+
+            Console.WriteLine("Are you alive?");
+            string alive = Console.ReadLine();
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Your name is " + name + "\nYour are " + age + " years old \nAlive? = " + alive);
+
         }
     }
 }
