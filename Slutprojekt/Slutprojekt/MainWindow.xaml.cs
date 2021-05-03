7db62b98c64f78ac25fd16be80bac8424c254a72
LeffeMannen <andreas.ohlsson@elev.ga.ntig.se>
2021-05-03 13:53:33 +0200

    e

--- Slutprojekt/Slutprojekt/MainWindow.xaml.cs
+++ Slutprojekt/Slutprojekt/MainWindow.xaml.cs
@@ -20,9 +20,76 @@ namespace Slutprojekt
     /// </summary>
     public partial class MainWindow : Window
     {
+        private UserControl gamePlan;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void content_Loaded(object sender, RoutedEventArgs e)
+        {
+            gamePlan = new UserControl();
+
+
+            Grid DynamicGrid = new Grid();
+
+            for (int i = 0; i < 11; i++)
+            {
+                var row = new RowDefinition();
+                row.Height = new GridLength(60);
+                DynamicGrid.RowDefinitions.Add(row);
+
+                var col = new ColumnDefinition();
+                col.Width = new GridLength(60);
+                DynamicGrid.ColumnDefinitions.Add(col);
+
+            }
+
+            for (int i = 4; i < 7; i++)
+            {
+                for (int j = 0; j < 12; j++)
+                {
+
+                    Ellipse rec = new Ellipse()
+                    {
+                        Fill = Brushes.White,
+                        Stroke = Brushes.Black,
+                        Width = 58,
+                        Height = 58,
+                    };
+                    Grid.SetRow(rec, i);
+                    Grid.SetColumn(rec, j);
+                    DynamicGrid.Children.Add(rec);
+
+                }
+            }
+
+            for (int i = 0; i < 12; i++)
+            {
+                for (int j = 4; j < 7; j++)
+                {
+                    Ellipse rec = new Ellipse()
+                    {
+                        Fill = Brushes.White,
+                        Stroke = Brushes.Black,
+                        Width = 58,
+                        Height = 58,
+                    };
+                    Grid.SetRow(rec, i);
+                    Grid.SetColumn(rec, j);
+                    DynamicGrid.Children.Add(rec);
+
+                }
+            }
+
+
+
+
+
+            gamePlan.Content = DynamicGrid;
+            content.Children.Add(gamePlan);
+
+
         }
     }
 }
